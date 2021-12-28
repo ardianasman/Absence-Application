3fa30352b4a4eb8ad11db85251ce6b161c63025b
Marcellino1067 <marcellino1067@gmail.com>
2021-12-28 08:50:18 +0700

    Account Pengurus

--- ProyekAndroid/app/src/main/java/com/example/proyekandroid/AccountPengurusActivity.kt
+++ ProyekAndroid/app/src/main/java/com/example/proyekandroid/AccountPengurusActivity.kt
@@ -1,11 +1,72 @@
 package com.example.proyekandroid
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyekandroid.MainActivity.Companion.data
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_account_pengurus.*
+import kotlinx.android.synthetic.main.editaccountdialog.view.*
 
 class AccountPengurusActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private lateinit var db:FirebaseFirestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_pengurus)
+        navBarAccPengurus.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homenav -> {
+                    val inthome = Intent(this@AccountPengurusActivity, HomePengurusActivity::class.java)
+                    startActivity(inthome)
+                }
+                R.id.inputnav -> {
+                    val intnot = Intent(this@AccountPengurusActivity, InputKaryawanActivity::class.java)
+                    startActivity(intnot)
+                }
+                R.id.calendarnav -> {
+                    val intcal = Intent(this@AccountPengurusActivity, CalendarActivity::class.java)
+                    startActivity(intcal)
+                }
+            }
+            true
+        }
+        db= FirebaseFirestore.getInstance()
+        db.collection("user").document(data).get()
+            .addOnSuccessListener {
+                tvNamePengurus.text = it.data?.get("name").toString()
+                tvRolePengurus.text=  it.data?.get("role").toString()
+                tvTelpPengurus.text = it.data?.get("telp").toString()
+            }
+        btnEditPengurus.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.editaccountdialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Edit Profile")
+                .setCancelable(false)
+            val etname = mDialogView.findViewById<EditText>(R.id.etNameDialog)
+            val ettelp = mDialogView.findViewById<EditText>(R.id.etTelpDialog)
+            etname.setText(tvNamePengurus.text.toString())
+            ettelp.setText(tvTelpPengurus.text.toString())
+            val mAlertDialog = mBuilder.show()
+
+            mDialogView.btnCancelDialog.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+            mDialogView.btnConfirmDialog.setOnClickListener {
+                db.collection("user").document(data)
+                    .update("name", etname.text.toString(),"telp", ettelp.text.toString())
+                mAlertDialog.dismiss()
+                db.collection("user").document(data)
+                    .get()
+                    .addOnSuccessListener {
+                        tvNamePengurus.text = it.data?.get("name").toString()
+                        tvRolePengurus.text=  it.data?.get("role").toString()
+                        tvTelpPengurus.text = it.data?.get("telp").toString()
+                    }
+            }
+        }
     }
 }
